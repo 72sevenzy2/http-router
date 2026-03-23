769fd7375aba9f94d27802695d4f58f9b558a6fa
72 <72sevenzy2@gmail.com>
2026-03-23 20:47:47 +0500

    finished implementing json responses + tested

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -30,13 +31,22 @@ func (s *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	handler(w, r)
 }
 
+// for json responding so the server can read the incoming info
 
+func JSON(w http.ResponseWriter, status int, data string) {
+	resp := map[string]interface{}{
+		"message": data,
+		"status":  status,
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
 
 func main() {
 	r := NewRouter()
 
 	r.Handle("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("test")
+		JSON(w, http.StatusOK, "test")
 	})
 	fmt.Println("api running")
 	http.ListenAndServe(":8080", r)
