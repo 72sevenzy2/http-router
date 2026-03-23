4073fa111ea85d50db84b6967c88aeff3769ab09
72 <72sevenzy2@gmail.com>
2026-03-23 20:18:54 +0500

    almost finished

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type Router struct {
+	routes map[string]http.HandlerFunc
+}
+
+func NewRouter() *Router {
+	return &Router{
+		routes: make(map[string]http.HandlerFunc),
+	}
+}
+
+func (r *Router) Handle(path string, handler http.HandlerFunc) {
+	r.routes[path] = handler
+}
+
+func (s *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	handler, ok := s.routes[r.URL.Path]
+
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+
+	handler(w, r)
+
+}
 
 func main() {
-	
+	fmt.Println("hi")
 }
