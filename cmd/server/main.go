5cec9f2fac0e73fa6216f8e87029d527a24b5dff
72 <72sevenzy2@gmail.com>
2026-03-24 17:05:54 +0500

    refactor/docs: add functional params patterns for JSON func + add comments for proper documentation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,22 +6,24 @@ import (
 	"net/http"
 )
 
-type Router struct {
+type Router struct { // initializing the router struct to hold all the routes
 	routes map[string]map[string]http.HandlerFunc
 }
 
 func NewRouter() *Router {
+	// contructing the router upon the func being called
 	return &Router{
 		routes: make(map[string]map[string]http.HandlerFunc),
 	}
 }
 
+// adding routes, and assigning the method of the route aswell as the url to the handler which then is executed in the ServeHTTP func
 func (r *Router) Handle(method, path string, handler http.HandlerFunc) {
 	if r.routes[path] == nil {
 		r.routes[path] = make(map[string]http.HandlerFunc)
 	}
 
-	r.routes[path][method] = handler
+	r.routes[path][method] = handler // assign both url and method to the handler (handler type is http.handlerFunc)
 }
 
 // core routing logic for my router
@@ -36,7 +38,7 @@ func (s *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	handler, ok := method[r.Method] // and check if the method is valid
 
 	if !ok {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		ERROR(w, "method not allowed")
 		return
 	}
 
@@ -45,21 +47,70 @@ func (s *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // for json responding so the server can read the incoming info
 
-func JSON(w http.ResponseWriter, status int, data string) {
-	// creating a temp response format for now
-	resp := map[string]any{ 
-		"message": data,
-		"status":  status,
+// configuration structs for json helpers below:
+
+// using function options to have optional parameters for main JSON helper func (this pattern is also known as the functional params pattern)
+// this pattern is also used by bigger http routers than mine like the chi or mux router.
+type JsonOptions struct { // this struct will be pointed from the ConfigOpts func
+	w      http.ResponseWriter
+	Data   interface{}
+	Status int
+}
+
+type ConfigOpts func(*JsonOptions) // any func which returns this type ONLY will use a pointer to the JsonOptions struct like used here
+
+// status param func
+func WithStatus(status int) ConfigOpts {
+	return func(jo *JsonOptions) {
+		jo.Status = status
+	}
+}
+
+// data param func
+func WithData(data any) ConfigOpts {
+	return func(jo *JsonOptions) {
+		jo.Data = data
+	}
+}
+
+func JSON(w http.ResponseWriter, opts ...ConfigOpts) {
+	// assigning the default values if i were to assign no params when calling the JSON func
+	options := &JsonOptions{ // these options will be replaced if there were opts included when calling this func with the data in those opts
+		w:      w,
+		Status: http.StatusOK,
+		Data:   nil,
 	}
 
-	json.NewEncoder(w).Encode(resp)
+	// initialising each opt to the appropriate param func
+	for _, opt := range opts {
+		opt(options) // each opt is a func that takes a pointer to the JsonOptions struct 
+	}
+
+	w.WriteHeader(options.Status)
+	w.Header().Set("Content-Type", "application/json")
+
+	if options.Data != nil {
+		json.NewEncoder(w).Encode(options.Data)
+	}
+}
+
+// error json response helper
+func ERROR(w http.ResponseWriter, data string) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Header().Set("Content-Content", "application/json")
+
+	JSON(w, WithStatus(http.StatusBadRequest), WithData(map[string]string{
+		"error": "bad request",
+	}))
 }
 
 func main() {
 	r := NewRouter()
 
 	r.Handle("GET", "/hi", func(w http.ResponseWriter, r *http.Request) {
-		JSON(w, http.StatusOK, "working api")
+		JSON(w, WithStatus(http.StatusOK), WithData(map[string]string{
+			"message": "sup",
+		}))
 	})
 	fmt.Println("api running")
 	http.ListenAndServe(":8080", r)
