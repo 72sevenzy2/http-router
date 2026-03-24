e882688db189925118752b2989507f4d4c0d6ffb
72 <72sevenzy2@gmail.com>
2026-03-24 14:39:17 +0500

    feat: add comments to expain the code more clearly

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -24,28 +24,30 @@ func (r *Router) Handle(method, path string, handler http.HandlerFunc) {
 	r.routes[path][method] = handler
 }
 
+// core routing logic for my router
 func (s *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	method, ok := s.routes[r.URL.Path]
+	method, ok := s.routes[r.URL.Path] // search for if the url path exists
 
 	if !ok {
 		http.NotFound(w, r)
 		return
 	}
 
-	handler, ok := method[r.Method]
+	handler, ok := method[r.Method] // and check if the method is valid
 
 	if !ok {
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	handler(w, r)
+	handler(w, r) // if all pass, then execute
 }
 
 // for json responding so the server can read the incoming info
 
 func JSON(w http.ResponseWriter, status int, data string) {
-	resp := map[string]any{
+	// creating a temp response format for now
+	resp := map[string]any{ 
 		"message": data,
 		"status":  status,
 	}
