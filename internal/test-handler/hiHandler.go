ccfd3ce03a5b7b8908d2f495ef2092be3747ca55
72 <72sevenzy2@gmail.com>
2026-03-27 14:53:00 +0500

    docs: add comments to further explain the code

--- internal/test-handler/hiHandler.go
+++ internal/test-handler/hiHandler.go
@@ -10,14 +10,14 @@ import (
 
 func hiHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var i entity.Entity
+		var i entity.Entity // initilising the entity
 
-		err := json.NewDecoder(r.Body).Decode(&i)
-		if err != nil {
+		err := json.NewDecoder(r.Body).Decode(&i) // decoding the body to get the data we want
+		if err != nil {                           // if there is no data which we needed in the body, throw an json error msg
 			response.ERROR(w, http.StatusBadRequest)
 			return
 		}
-
+		// respond with json returning the users user and the users id
 		response.JSON(w, response.WithStatus(http.StatusOK), response.WithData(map[string]interface{}{
 			"User": i.User,
 			"Id":   i.Id,
