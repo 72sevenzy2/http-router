fa7137817b65be3eb70bb0a3bd15269a4c55d3b6
72 <72sevenzy2@gmail.com>
2026-03-28 13:18:34 +0500

    feat: add json handler functions option param helper

---
+++ internal/response/helpers/options.go
@@ -0,0 +1,14 @@
+package response
+
+import (
+	"github.com/72sevenzy2/http-router/internal/response"
+	"net/http"
+)
+
+func OK(w http.ResponseWriter, data any) {
+	response.JSON(w, response.WithData(data), response.WithStatus(http.StatusOK))
+}
+
+func FAILED(w http.ResponseWriter, status int, msg string) {
+	response.JSON(w, response.WithStatus(status), response.WithError(msg))
+}
