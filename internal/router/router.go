e9bf48cebe589b19d658f9207bbfdb8e1a6cb2ad
72 <72sevenzy2@gmail.com>
2026-03-30 13:43:46 +0500

    refactor: split middleware code into different file

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,9 +1,7 @@
 package router
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/72sevenzy2/http-router/internal/response/helpers"
 )
@@ -11,54 +9,6 @@ import (
 type Router struct { // initializing the router struct to hold all the routes
 	Routes      map[string]map[string]http.HandlerFunc
 	Middlewares []Middleware // storing our middlewares here (type is our Middleware function type)
-}
-
-type Middleware func(http.HandlerFunc) http.HandlerFunc // the middleware type (takes in the current handler and returns a new one)
-
-// middleware (logger)
-func Logger() Middleware { // returns the middleware type (which takes in a handler and returns a new one)
-	return func(hf http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("Request has started with method: ", r.Method)
-
-			hf(w, r)
-
-			fmt.Println("Request has ended")
-		}
-	}
-}
-
-// auth middleware (no real authorization yet but will be adding this for a test case)
-
-func Auth(AuthKey string) Middleware {
-	return func(hf http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			authLab := r.Header.Get("Authorization") // grabbing the token
-
-			token := strings.TrimPrefix(authLab, "Bearer ") // removing the "bearer " part of the token to then compare it to the authkey
-
-			if token == authLab || token != AuthKey { // check if the authkey is matching
-				helpers.FAILED(w, http.StatusForbidden, "Invalid Token") // if not then throw a failed json response
-				return
-			}
-
-			hf(w, r)
-		}
-	}
-}
-
-// Use func to use the middewares (also appending it to the Middlewares type in router struct
-func (r *Router) Use(s Middleware) {
-	r.Middlewares = append(r.Middlewares, s)
-}
-
-// func to apply the middlewares
-func (r *Router) applyMiddlewares(h http.HandlerFunc) http.HandlerFunc {
-	for i := len(r.Middlewares) - 1; i >= 0; i-- {
-		h = r.Middlewares[i](h)
-	}
-
-	return h
 }
 
 func NewRouter() *Router {
@@ -80,6 +30,7 @@ func (r *Router) Handle(method, path string, handler http.HandlerFunc) {
 	//                       "GET": "and some handler here, (in this case, it will be the http handlerfunc we used)"
 }
 
+
 // core routing logic for my router
 func (s *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	method, ok := s.Routes[r.URL.Path] // search for if the url path exists
@@ -96,7 +47,8 @@ func (s *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	wra := s.applyMiddlewares(handler)
+	wra := s.ApplyMiddlewares(handler)
 	wra(w, r)
 
 }
+
