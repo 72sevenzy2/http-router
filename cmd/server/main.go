56b31cad9a3bb5a9a99fe56ad48572ba99bc641f
72 <72sevenzy2@gmail.com>
2026-04-01 15:04:01 +0500

    refactor: remove redundant hi route handler + move recoverer mw injection to be executed before other middlewares

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,19 +10,14 @@ import (
 func main() {
 	r := router.NewRouter()
 
-	r.Use(router.BasicAuth("user", "hi"))
+
 	r.Use(router.Recoverer())
+	r.Use(router.BasicAuth("user", "hi"))
 	r.Use(router.Logger())
 
 	r.Handle(http.MethodGet, "/p", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("responded"))
 	})
-
-	go func() {
-		r.Handle(http.MethodGet, "/hi", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("hello user"))
-		})
-	}()
 
 	fmt.Println("server running on port 8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
