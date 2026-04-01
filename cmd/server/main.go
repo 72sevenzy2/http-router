f20ce094ee15c8604ec16843d0554b35cde724e5
72 <72sevenzy2@gmail.com>
2026-04-01 14:57:45 +0500

    test: test basic auth

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,20 +5,27 @@ import (
 	"net/http"
 
 	"github.com/72sevenzy2/http-router/internal/router"
-	"github.com/72sevenzy2/http-router/internal/test-handler"
 )
 
 func main() {
 	r := router.NewRouter()
 
+	r.Use(router.BasicAuth("user", "hi"))
 	r.Use(router.Recoverer())
 	r.Use(router.Logger())
-	r.Use(router.Auth("siam"))
 
-	r.Handle(http.MethodPost, "/p", handler.HiHandler())
+	r.Handle(http.MethodGet, "/p", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("responded"))
+	})
+
+	go func() {
+		r.Handle(http.MethodGet, "/hi", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("hello user"))
+		})
+	}()
 
 	fmt.Println("server running on port 8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
-		panic(err);
+		panic(err)
 	}
 }
